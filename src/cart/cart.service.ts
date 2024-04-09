3573ee62910ea9a085fa6d504ee9e9e5dcc96bcb
Sammed S P <127593704+psammy171@users.noreply.github.com>
2024-04-09 10:51:40 +0530

    feat implement cart apis
    added apis for adding and removing items and checking out cart

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -5,6 +5,23 @@ import { PrismaService } from 'src/global/prisma/prisma.service';
 export class CartService {
   constructor(private prisma: PrismaService) {}
 
+  async getCartItems(userId: string) {
+    const cart = await this.prisma.userOrder.findFirst({
+      where: {
+        isCart: true,
+        userId: userId,
+      },
+      include: {
+        items: {
+          include: {
+            item: true,
+          },
+        },
+      },
+    });
+    return cart?.items || [];
+  }
+
   async addItemToCart(userId: string, itemId: string) {
     const item = await this.prisma.groceryItem.findUnique({
       where: {
@@ -50,10 +67,77 @@ export class CartService {
           groceryItemId: itemId,
         },
         update: {
-          quantity: 1,
+          quantity: { increment: 1 },
           groceryItemId: itemId,
         },
       });
     }
+    return {
+      message: 'Item added to cart',
+    };
+  }
+
+  async removeItemFromCart(userId: string, itemId: string) {
+    const cart = await this.prisma.userOrder.findFirst({
+      where: {
+        AND: {
+          userId: userId,
+          isCart: true,
+        },
+      },
+      include: {
+        items: true,
+      },
+    });
+    const item = await this.prisma.orderItem.findUnique({
+      where: {
+        groceryItemId_orderId: {
+          orderId: cart.id,
+          groceryItemId: itemId,
+        },
+      },
+    });
+    if (item.quantity === 1) {
+      await this.prisma.orderItem.delete({
+        where: {
+          groceryItemId_orderId: {
+            orderId: cart.id,
+            groceryItemId: itemId,
+          },
+        },
+      });
+    } else {
+      await this.prisma.orderItem.update({
+        where: {
+          groceryItemId_orderId: {
+            orderId: cart.id,
+            groceryItemId: itemId,
+          },
+        },
+        data: {
+          quantity: { decrement: 1 },
+        },
+      });
+    }
+    return {
+      message: 'Item removed from cart',
+    };
+  }
+
+  async checkoutCartItems(userId: string) {
+    const cart = await this.prisma.userOrder.updateMany({
+      where: {
+        isCart: true,
+        userId: userId,
+      },
+      data: {
+        isCart: false,
+      },
+    });
+    console.log('Cart', cart);
+    if (cart.count === 0) throw new BadRequestException();
+    return {
+      message: 'Cart checked out',
+    };
   }
 }
