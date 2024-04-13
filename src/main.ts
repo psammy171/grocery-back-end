2883f35def90d978c7b0b78b4bb6ac9274b8495b
Sammed S P <127593704+psammy171@users.noreply.github.com>
2024-04-14 02:01:17 +0530

    feat cors origin
    added front-end url as cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: process.env.FRONT_END_URL,
+  });
   await app.listen(8080);
 }
 bootstrap();
